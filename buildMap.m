a36456b2b42f6c87438b5b02a8c7412c79951f4e
coxlaurel <146145083+coxlaurel@users.noreply.github.com>
2024-05-01 23:57:46 -0400

    Update buildMap.m

--- buildMap.m
+++ buildMap.m
@@ -1,5 +1,5 @@
 function buildMap()
-    % Load data from 'valorant2.mat'
+    % Load data from 'valorant3.mat'
     load('valorant3','angles','positions','scans');
 
     % Create a figure to hold all the subplots
@@ -66,24 +66,4 @@ function buildMap()
 
     % Save global points
     save('global_points.mat', 'global_points');
-end
-
-function [v] = functionFromPoints(v, x, y, x_coord, y_coord)
-
-end
-
-function [v] = build_equation(v, x, y, x_coord, y_coord)
-% Add new source to equation evaluted at meshgrid.
-%
-% Inputs
-%   v (square matrix): Equation evaluted at meshgrid
-%   x (square matrix): X coordinates of meshgrid
-%   y (square matric): Y coordinates of meshgrid
-%   x_coord (float): X coordinate of source
-%   y_coord(float): Y coordinate of source
-%
-% Returns
-%   v (square matrix): Newly built equation evaluted at meshgrid
-
-    v = v - 0.16 * (log(sqrt((x-x_coord).^2+(y-y_coord).^2))); %had at 0.1 for vid 4/29
 end
